52617083f0a9b82973b2fc9839f797d5617c2ebf
mu-az88 <azoozrock@gmail.com>
2025-10-10 17:19:47 +0200

    I learned reusable buttons

--- lib/Navgation Bar/favorites_nav.dart
+++ lib/Navgation Bar/favorites_nav.dart
@@ -6,7 +6,7 @@ class FavoritesNav extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
+        backgroundColor: Color.fromARGB(255, 229, 243, 255),
         appBar: AppBar(
           title: Center(
               child: Text('Favorites', style: TextStyle(color: Colors.black))),
@@ -19,11 +19,39 @@ class FavoritesNav extends StatelessWidget {
               SizedBox(height: 94),
               Row(
                 children: [
-                  FavoritesItem(),
-                  FavoritesItem(),
-                  FavoritesItem(),
+                  appButton(
+                    backgroundColor: Colors.white,
+                    textColor: Color.fromARGB(255, 0, 59, 115),
+                    icon: Icons.home_rounded,
+                    text: 'Home',
+                    size: 110,
+                  ),
+                  appButton(
+                    backgroundColor: Colors.white,
+                    textColor: Color.fromARGB(255, 0, 59, 115),
+                    icon: Icons.work,
+                    text: 'Work',
+                    size: 110,
+                  ),
+                  appButton(
+                    backgroundColor: Colors.white,
+                    textColor: Color.fromARGB(255, 0, 59, 115),
+                    icon: Icons.local_gas_station_rounded,
+                    text: 'Station',
+                    size: 110,
+                  ),
                 ],
-              )
+              ),
+              Row(
+                children: [
+                  appButton(
+                      backgroundColor: Colors.white,
+                      textColor: Color.fromARGB(255, 255, 167, 38),
+                      icon: Icons.add,
+                      text: 'Add',
+                      size: 110),
+                ],
+              ),
             ],
           ),
         ));
@@ -49,21 +77,53 @@ class FavoritesItem extends StatelessWidget {
 class appButton extends StatelessWidget {
   final Color backgroundColor;
   final Color textColor;
-  final Color borderColor;
+  final Color? borderColor;
   final String text;
   final IconData? icon;
-  double size;
+  final double size;
   appButton(
       {super.key,
       required this.backgroundColor,
       required this.textColor,
-      required this.borderColor,
+      this.borderColor,
       required this.text,
       required this.size,
       this.icon});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      margin: EdgeInsets.fromLTRB(15, 30, 0, 0),
+      width: size,
+      height: size,
+      child: Column(
+        children: [
+          SizedBox(height: 20),
+          Center(
+            child: Icon(
+              icon,
+              color: textColor,
+              size: 50,
+            ),
+          ),
+          SizedBox(height: 10),
+          Center(
+            child: Text(text),
+          ),
+        ],
+      ),
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 2,
+            blurRadius: 5,
+            offset: Offset(0, 3), // changes position of shadow
+          ),
+        ],
+        color: backgroundColor,
+        borderRadius: BorderRadius.circular(10),
+      ),
+    );
   }
 }
